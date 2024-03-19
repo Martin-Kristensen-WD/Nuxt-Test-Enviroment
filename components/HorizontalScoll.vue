20b5c5756e6f189aa7cb5013875f209e23cd59f7
Martin Kristensen <martin.kristensen@aller.com>
2024-03-19 11:00:02 +0100

    testing a horizontal scroll component

---
+++ components/HorizontalScoll.vue
@@ -0,0 +1,33 @@
+<template>
+  <main ref="scrollContainer" class="overflow-x-hidden flex">
+    <section class="min-w-[50vw] min-h-[90vh] grid text-center items-center text-4xl">
+      <h1>Beep</h1>
+    </section>
+    <section class="min-w-[50vw] min-h-[90vh] grid text-center items-center text-4xl bg-slate-500">
+      <h1>Boop</h1>
+    </section>
+    <section class="min-w-[50vw] min-h-[90vh] grid text-center items-center text-4xl">
+      <h1>Boooom</h1>
+    </section>
+    <section class="min-w-[50vw] min-h-[90vh] grid text-center items-center text-4xl bg-slate-500">
+      <h1>The End</h1>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+const scrollContainer = ref<HTMLElement | null>(null)
+
+function handleWheel(event: WheelEvent) {
+  event.preventDefault()
+  if (scrollContainer.value) {
+    scrollContainer.value.scrollLeft += event.deltaY
+  }
+}
+
+onMounted(() => {
+  scrollContainer.value?.addEventListener("wheel", handleWheel);
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold text-center mt-20">INDEX PAGE</h1>
+    <HorizontalScoll />
+    <div class="min-h-[90vh] min-w-[100vw] bg-teal-400">YO</div>
   </div>
 </template>
 
